2c740b28f6e7d14186cd890196c98dd255aef3a1
kvnx <35074883+kvnx@users.noreply.github.com>
2019-11-30 13:47:49 -0600

    basic setup

---
+++ jahcord/src/main/java/systems/kvnx/jahcord/JahcordMain.java
@@ -0,0 +1,17 @@
+package systems.kvnx.jahcord;
+
+/**
+ * 
+ * @author kvnx
+ *
+ */
+
+public class JahcordMain {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
---
+++ jahcord/src/main/java/systems/kvnx/jahcord/Login.java
@@ -0,0 +1,24 @@
+package systems.kvnx.jahcord;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ * @author kvnx
+ *
+ */
+
+public class Login extends JFrame {
+	
+	private String token;
+	
+	public Login() {
+		
+		token = "";
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+	}
+
+}
