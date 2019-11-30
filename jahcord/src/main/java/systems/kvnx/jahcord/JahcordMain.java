5c86ef4157500a0cea2f22d7ca603542fcfa0f06
kvnx <35074883+kvnx@users.noreply.github.com>
2019-11-30 15:06:12 -0600

    login form and token format checker

--- jahcord/src/main/java/systems/kvnx/jahcord/JahcordMain.java
+++ jahcord/src/main/java/systems/kvnx/jahcord/JahcordMain.java
@@ -1,15 +1,50 @@
 package systems.kvnx.jahcord;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+
 /**
  * 
  * @author kvnx
+ * This class handles logging in and redirecting to the client
  *
  */
 
 public class JahcordMain {
 	
+	static String token;
+	
 	public static void main(String[] args) {
 		
+		FlatLaf theme = new FlatLightLaf();
+		
+		LoginForm loginForm = new LoginForm(theme);
+		loginForm.addWindowListener(new WindowListener() {
+
+			@Override
+			public void windowActivated(WindowEvent arg0) {}
+
+			@Override
+			public void windowClosed(WindowEvent arg0) {
+				token = loginForm.getToken();
+				Login login = new Login(token);
+			}
+
+			@Override
+			public void windowClosing(WindowEvent arg0) {}
+			@Override
+			public void windowDeactivated(WindowEvent arg0) {}
+			@Override
+			public void windowDeiconified(WindowEvent arg0) {}
+			@Override
+			public void windowIconified(WindowEvent arg0) {} 
+			@Override
+			public void windowOpened(WindowEvent arg0) {}
+			
+		});
 		
 		
 	}
--- jahcord/src/main/java/systems/kvnx/jahcord/Login.java
+++ jahcord/src/main/java/systems/kvnx/jahcord/Login.java
@@ -1,24 +1,10 @@
 package systems.kvnx.jahcord;
 
-import javax.swing.JFrame;
-
-/**
- * 
- * @author kvnx
- *
- */
-
-public class Login extends JFrame {
+public class Login {
 	
 	private String token;
-	
-	public Login() {
-		
-		token = "";
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-		
+	public Login (String token) {
+		this.token = token;
 	}
 
 }
